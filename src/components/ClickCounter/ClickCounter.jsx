af5839767b720d7524a59c74423465a7b1ec7706
penko <odael.odes@gmail.com>
2018-11-20 11:20:02 +0300

    [click_counter] Make it react!

--- src/components/ClickCounter/ClickCounter.jsx
+++ src/components/ClickCounter/ClickCounter.jsx
@@ -1,39 +1,65 @@
 import React, { Component } from 'react'
 import Btn from 'components/Btn'
 
+const initialState = {  maxValue: 20, clicks: 0, step: 1  }
+
 export default class ClickCounter extends Component {
+  state = {...initialState}
+
+  handleClick = () => {
+    this.setState({
+      clicks: (this.state.clicks + this.state.step <= this.state.maxValue) ? this.state.clicks + this.state.step : this.state.clicks
+    })
+  }
+
+  handleIncreaseMax = () => {
+    this.setState({
+      maxValue: this.state.maxValue + 1
+    })
+  }
+
+  handleIncreaseStep = () => {
+    this.setState({
+      step: this.state.step + 1
+    })
+  }
+
+  handleReset = () => {
+    this.setState(initialState)
+  }
+
   render() {
     return (
-      <div class="row justify-center p-t bg-tertiary section">
-        <div class="card standard-border card-narrow shadow">
-          <div class="info">
+      <div className="row justify-center p-t bg-tertiary section">
+        <div className="card standard-border card-narrow shadow">
+          <div className="info">
             <div>Clicks</div>
 
-            <div class="m-t">
-              <span class="info-key">Max Value:</span>
-              <span class="text-accent">0</span>
+            <div className="m-t">
+              <span className="info-key">Max Value:</span>
+              <span className="text-accent">{ this.state.maxValue }</span>
             </div>
 
-            <div class="m-t">
-              <span class="info-key">clicks:</span>
-              <span class="text-accent">0</span>
+            <div className="m-t">
+              <span className="info-key">clicks:</span>
+              <span className="text-accent">{ this.state.clicks }</span>
             </div>
 
-            <div class="m-t">
-              <span class="info-key">clicks left:</span>
-              <span class="text-accent">0</span>
+            <div className="m-t">
+              <span className="info-key">clicks left:</span>
+              <span className="text-accent">{ this.state.maxValue - this.state.clicks }</span>
             </div>
 
-            <div class="m-t">
-              <span class="info-key">step:</span>
-              <span class="text-accent">1</span>
+            <div className="m-t">
+              <span className="info-key">step:</span>
+              <span className="text-accent">{ this.state.step }</span>
             </div>
           </div>
 
-          <Btn className="m-t d-block">Click</Btn>
-          <Btn className="m-t d-block">Reset</Btn>
-          <Btn className="m-t d-block">Increase Step</Btn>
-          <Btn className="m-t d-block">Increase Max</Btn>
+          <Btn onClick={ this.handleClick } className="m-t d-block" disabled={ this.state.clicks === this.state.maxValue }>Click</Btn>
+          <Btn onClick={ this.handleReset } className="m-t d-block">Reset</Btn>
+          <Btn onClick={ this.handleIncreaseStep } className="m-t d-block">Increase Step</Btn>
+          <Btn onClick={ this.handleIncreaseMax } className="m-t d-block">Increase Max</Btn>
         </div>
       </div>
     )
