fd3bbd5650ce40f52db837ba0263c445b5f3bc4c
penko <odael.odes@gmail.com>
2018-11-23 22:52:34 +0300

    [click_counter] Review
    Add destructuring assignment.
    Remove unnecessary ternary operator.

--- src/components/ClickCounter/ClickCounter.jsx
+++ src/components/ClickCounter/ClickCounter.jsx
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
 import Btn from 'components/Btn'
 
-const initialState = {  maxValue: 20, clicks: 0, step: 1  }
+const initialState = { maxValue: 20, clicks: 0, step: 1 }
 
 export default class ClickCounter extends Component {
-  state = {...initialState}
+  state = { ...initialState }
 
   handleClick = () => {
-    this.setState({
-      clicks: (this.state.clicks + this.state.step <= this.state.maxValue) ? this.state.clicks + this.state.step : this.state.clicks
-    })
+    if (this.state.clicks + this.state.step <= this.state.maxValue) {
+      this.setState({
+        clicks: this.state.clicks + this.state.step,
+      })
+    }
   }
 
   handleIncreaseMax = () => {
     this.setState({
-      maxValue: this.state.maxValue + 1
+      maxValue: this.state.maxValue + 1,
     })
   }
 
   handleIncreaseStep = () => {
     this.setState({
-      step: this.state.step + 1
+      step: this.state.step + 1,
     })
   }
 
@@ -29,6 +31,7 @@ export default class ClickCounter extends Component {
   }
 
   render() {
+    const { maxValue, clicks, step } = this.state
     return (
       <div className="row justify-center p-t bg-tertiary section">
         <div className="card standard-border card-narrow shadow">
@@ -37,29 +40,29 @@ export default class ClickCounter extends Component {
 
             <div className="m-t">
               <span className="info-key">Max Value:</span>
-              <span className="text-accent">{ this.state.maxValue }</span>
+              <span className="text-accent">{maxValue}</span>
             </div>
 
             <div className="m-t">
               <span className="info-key">clicks:</span>
-              <span className="text-accent">{ this.state.clicks }</span>
+              <span className="text-accent">{clicks}</span>
             </div>
 
             <div className="m-t">
               <span className="info-key">clicks left:</span>
-              <span className="text-accent">{ this.state.maxValue - this.state.clicks }</span>
+              <span className="text-accent">{maxValue - clicks}</span>
             </div>
 
             <div className="m-t">
               <span className="info-key">step:</span>
-              <span className="text-accent">{ this.state.step }</span>
+              <span className="text-accent">{step}</span>
             </div>
           </div>
 
-          <Btn onClick={ this.handleClick } className="m-t d-block" disabled={ this.state.clicks === this.state.maxValue }>Click</Btn>
-          <Btn onClick={ this.handleReset } className="m-t d-block">Reset</Btn>
-          <Btn onClick={ this.handleIncreaseStep } className="m-t d-block">Increase Step</Btn>
-          <Btn onClick={ this.handleIncreaseMax } className="m-t d-block">Increase Max</Btn>
+          <Btn onClick={this.handleClick} className="m-t d-block" disabled={clicks === maxValue}>Click</Btn>
+          <Btn onClick={this.handleReset} className="m-t d-block">Reset</Btn>
+          <Btn onClick={this.handleIncreaseStep} className="m-t d-block">Increase Step</Btn>
+          <Btn onClick={this.handleIncreaseMax} className="m-t d-block">Increase Max</Btn>
         </div>
       </div>
     )
