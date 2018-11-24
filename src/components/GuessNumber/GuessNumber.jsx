72f9a3088a0f28040fc0a99e8daecd21279defc6
penko <odael.odes@gmail.com>
2018-11-24 22:08:18 +0300

    [guess_number] Handle input onChange

--- src/components/GuessNumber/GuessNumber.jsx
+++ src/components/GuessNumber/GuessNumber.jsx
@@ -1,23 +1,43 @@
 import React, { Component } from 'react'
 import Btn from 'components/Btn'
-import TextInput from 'components/NumberInput'
+import NumberInput from 'components/NumberInput'
 
-const initialState = { round: 1, step: 1, maxSteps: 7, maxNumber: 100, number: null, infoText: 'Try to guess it!' }
+const initialState = {
+  round: 1,
+  step: 1,
+  maxSteps: 7,
+  maxNumber: 100,
+  number: null,
+  realNumber: null,
+  infoText: 'Try to guess it!',
+}
 
 export default class GuessNumber extends Component {
   state = { ...initialState }
 
   constructor(props) {
     super(props)
-    this.state.number = this.getRandomNumber()
+    this.state.realNumber = this.getRandomRealNumber()
   }
 
-  getRandomNumber = () => {
+  getRandomRealNumber = () => {
     return Math.floor(Math.random() * this.state.maxNumber)
   }
 
+  handleTry = () => {
+    this.setState(
+
+    )
+  }
+
+  updateNumber = (evt) => {
+    this.setState({
+      number: evt.target.value
+    });
+  }
+
   render() {
-    const { round, step, maxSteps, maxNumber, number, infoText } = this.state
+    const { round, step, maxSteps, maxNumber, number, realNumber, infoText } = this.state
     return (
       <div className="row justify-center p-t bg-tertiary section">
         <div className="card standard-border card-narrow shadow">
@@ -28,10 +48,13 @@ export default class GuessNumber extends Component {
               <span className="info-key">Info:</span>
               <span className="text-accent">{infoText}</span>
             </div>
+
           </div>
           <div>
-            <TextInput className="fancy-input m-t"/>
-            <Btn onClick={this.handleIncreaseMax} className="m-t d-block">Try!</Btn>
+
+            <NumberInput onChange={this.updateNumber} className="fancy-input m-t"/>
+            <Btn onClick={this.handleTry} className="m-t d-block">Try!</Btn>
+
           </div>
         </div>
       </div>
